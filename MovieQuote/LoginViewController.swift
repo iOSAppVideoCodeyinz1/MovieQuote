dcfb9008d17fa0a3bdf1bf1f8a7cbd29819ccff2
rhit-yinz1 <yinz1@rose-hulman.edu>
2021-07-29 15:19:08 -0400

    sign up/ log in with emails and password

--- MovieQuote/LoginViewController.swift
+++ MovieQuote/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     let ShowListSegueIdentifier = "ShowListSegue"
@@ -13,10 +14,43 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.placeholder = "Email"
+        passwordTextField.placeholder = "Password"
+    }
+    
     @IBAction func pressedSignInNewUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error creating a new user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked, new user is created")
+            print("Email is \(authResult!.user.email)")
+            print("UID is \(authResult!.user.uid)")
+
+
+
+        }
     }
     
     @IBAction func pressedLogInExistingUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error logging in an existing user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked, logged in an existion account")
+            print("Email is \(authResult!.user.email)")
+            print("UID is \(authResult!.user.uid)")
+        }
     }
     
     
